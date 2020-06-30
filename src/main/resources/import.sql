546844ef5d1e4bcc00612ae716a4d51be41e5d10
s0m05lw <s0m05lw@LEUSX1909154751.homeoffice.Wal-Mart.com>
2020-06-30 17:29:22 -0500

    CORE-1826, #comment - added impl file

--- src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO Card (cardId, name, expirationMonth, expirationyear) VALUES (11,'Sai', '12', '2025');
-INSERT INTO Card (cardId, name, expirationMonth, expirationyear) VALUES (12, 'Adam', '10', '2021');
-INSERT INTO Card (cardId, name, expirationMonth, expirationyear) VALUES (13, 'Bob', '05', '2023');
--- src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS Card;
- 
-CREATE TABLE Card (
-  cardId INT AUTO_INCREMENT  PRIMARY KEY,
-  name VARCHAR(250) NOT NULL,
-  expirationMonth INT NOT NULL,
-  expirationyear INT DEFAULT NULL
-);
