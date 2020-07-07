9309bbdd7faa9114532665216a5968a95a6570f4
s0m05lw <s0m05lw@LEUSX1909154751.homeoffice.Wal-Mart.com>
2020-07-07 13:31:36 -0500

    CORE-2039, #comment - updated one

--- src/main/java/com/walmart/gai/security/SecurityFilter.java
+++ src/main/java/com/walmart/gai/security/SecurityFilter.java
@@ -40,6 +40,7 @@ public class SecurityFilter extends GenericFilterBean{
 			}else {
 				((HttpServletResponse) response).setStatus(HttpStatus.UNAUTHORIZED.value());
 				((HttpServletResponse) response).setContentType(Constants.CONTENTTYPE);
+				((HttpServletResponse) response).setHeader("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
 				ErrorResponseDTO errorResponse = new ErrorResponseDTO(HttpStatus.UNAUTHORIZED,String.valueOf(HttpStatus.UNAUTHORIZED.value()),Constants.AUTHENTICATIONMESSAGE, req.getContextPath());
 		        PrintWriter out = response.getWriter();
 		        out.print(objectMapper.writeValueAsString(errorResponse));
